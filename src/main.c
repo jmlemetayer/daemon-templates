912e98ae01c71d14c423b6627b399d37c20eb829
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2016-09-06 11:50:39 +0200

    Add a foreground option

--- src/main.c
+++ src/main.c
@@ -31,32 +31,41 @@ static void handler(int signal)
 
 int main(int argc, char **argv)
 {
+	int logopt = LOG_PID;
+	int foreground = 0;
 	int opt;
 
 	const struct option lopt[] = {
+		{"foreground",	no_argument, NULL, 'f'},
 		{"version",	no_argument, NULL, 'v'},
 		{"help",	no_argument, NULL, 'h'},
 		{NULL,		0, NULL, 0},
 	};
 
-	while ((opt = getopt_long(argc, argv, "vh", lopt, NULL)) != EOF) {
+	while ((opt = getopt_long(argc, argv, "fvh", lopt, NULL)) != EOF) {
 		switch (opt) {
+		case 'f':
+			logopt |= LOG_PERROR;
+			foreground = 1;
+			break;
+
 		case 'v':
 			fprintf(stdout, "daemon %s\n", VERSION);
 			exit(EXIT_SUCCESS);
 
 		default:
 			fprintf(stderr, "Usage: daemon [OPTIONS]...\n"
+			        "-f, --foreground  Do not daemonize.\n"
 			        "-v, --version     Display version.\n"
 			        "-h, --help        Display this help screen.\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 
-	openlog("daemon", LOG_PID, LOG_DAEMON);
+	openlog("daemon", logopt, LOG_DAEMON);
 	info("Starting daemon");
 
-	if (daemon(0, 0) < 0) {
+	if (foreground == 0 && daemon(0, 0) < 0) {
 		ecritical("Failed to daemonize");
 		exit(EXIT_FAILURE);
 
